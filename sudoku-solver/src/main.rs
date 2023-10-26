5b7cd7fe601f693075ad6999727349a8ae40b053
Anders-EM <kanders.emrich@gmail.com>
2023-10-26 13:35:48 -0500

    solved

--- sudoku-solver/src/main.rs
+++ sudoku-solver/src/main.rs
@@ -6,4 +6,16 @@ fn main() {
     sudoku.load_from_file(String::from(
         "/Users/kurt/Desktop/GHProjs/sudoku-rust/txt/sudoku-test1.txt",
     ));
+    let mut solved = sudoku::Sudoku::new();
+    solved.load_from_file(String::from(
+        "/Users/kurt/Desktop/GHProjs/sudoku-rust/txt/sudoku-test1-solution.txt",
+    ));
+
+    sudoku.solve();
+
+    print!("Solved sudoku:\n");
+
+    sudoku.print();
+
+    print!("{} ", sudoku.equals(&solved));
 }
--- sudoku-solver/src/sudoku.rs
+++ sudoku-solver/src/sudoku.rs
@@ -22,6 +22,54 @@ impl Sudoku {
         true
     }
 
+    pub fn does_num_exist(&self, row: i32, col: i32, num: i32) -> bool {
+        // checking column
+        for temp_row in 0..9 {
+            if self.board[temp_row][col as usize] == num {
+                return true;
+            }
+        }
+
+        // checking row
+        for temp_col in 0..9 {
+            if self.board[row as usize][temp_col] == num {
+                return true;
+            }
+        }
+
+        // check the grid the number is in
+        let box_start_row: usize = (row - row % 3) as usize;
+        let box_start_col: usize = (col - col % 3) as usize;
+
+        for temp_row in 0..3 {
+            for temp_col in 0..3 {
+                if self.board[box_start_row + temp_row][box_start_col + temp_col] == num {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    pub fn is_num_valid(&self, row: i32, col: i32, num: i32) -> bool {
+        !self.does_num_exist(row, col, num)
+    }
+
+    pub fn does_empty_exist(&self, row: &mut i32, col: &mut i32) -> bool {
+        for i in 0..9 {
+            for j in 0..9 {
+                if self.board[i][j] == 0 {
+                    *row = i as i32;
+                    *col = j as i32;
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
     pub fn load_from_file(&mut self, filename: String) -> io::Result<()> {
         let file = File::open(&filename)?;
         let reader = BufReader::new(file);
@@ -48,5 +96,34 @@ impl Sudoku {
         }
 
         Ok(()) // Explicitly return an 'Ok' variant to indicate success
+    }
+
+    pub fn solve(&mut self) -> bool {
+        let mut row: i32 = 0;
+        let mut col: i32 = 0;
+
+        if !self.does_empty_exist(&mut row, &mut col) {
+            return true;
+        }
+        for num in 1..10 {
+            if self.is_num_valid(row, col, num) {
+                self.board[row as usize][col as usize] = num;
+                if self.solve() {
+                    return true;
+                }
+                self.board[row as usize][col as usize] = 0;
+            }
+        }
+
+        false
+    }
+
+    pub fn print(&self) {
+        for row in &self.board {
+            for &num in row {
+                print!("{} ", num);
+            }
+            println!();
+        }
     }
 }
